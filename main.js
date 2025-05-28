d315f600372ec6669872f4c1c7420b3d0df5fb19
Calizaya Fernando <fernandocalizaya@gmail.com>
2025-05-28 12:38:40 +0200

    write coments

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const a = "adios";
 
 console.log(a);
+
+// para subir haces un commit con el nombre 
+// enlazas $ git remote add origin https://github.com/spidey-177/amazing-git.git
+// y luego $ git push -u origin master
