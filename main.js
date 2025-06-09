0701d68f27860136364d0ff83e50c0c5ffb13bf7
Calizaya Fernando <fernandocalizaya@gmail.com>
2025-06-09 23:05:52 +0200

    ramas..

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ console.log(a+"aaaaaaaaa");
 // y luego $ git push -u origin master
 
 // git pull para actualizar a la version mas reciente
+// como crear ramas? pues git checkout -b login , esto ultimo es ejemploo 
+// a lado de la direccion del archivo deberia aparecer (login)
+
+// para ver las ramas? git branch
+
+// las ramas aun no estaban en el github , en las izquierda superior solo sale la rama master entonces.. debemos darle la 
+// direccion de nuevooo
+
+// una vez ya actualizada la rama con su direccion github no es necesario ponerlo de nuevoo
+// como moverse a la rama master de nuevo? git checkout master
