6b2dd2561638cb5e2ec3e12d2a026a6fc3a5f3be
Calizaya Fernando <fernandocalizaya@gmail.com>
2025-06-12 12:41:21 +0200

    last-spidey

--- new-main.js
+++ new-main.js
@@ -1,1 +1,5 @@
 // login for new
+
+// hacer siempre un pull request en github para trbajar en equipo
+
+// asi tambien se puede usar el git push origin "nombre rama" pero en el video dice que no es muy  comodo
